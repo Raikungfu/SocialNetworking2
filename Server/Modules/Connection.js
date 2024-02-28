8f0753783f90520d2326e6a53ade8dfc66e289fc
Rai Yugi <raikungfu98@gmail.com>
2024-02-29 00:34:56 +0700

    test deploy 3

--- Server/Modules/Connection.js
+++ Server/Modules/Connection.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/SocialNetworking', {
-    useNewUrlParser: true
-})
+mongoose.connect(
+  "mongodb+srv://raikungfu:SaintRaiYugi@cluster0.qpgarnj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useNewUrlParser: true,
+  }
+);
 
-mongoose.connection.on('err', (err) => {
-    console.log(err)
-})
+mongoose.connection.on("err", (err) => {
+  console.log(err);
+});
 
 module.exports = mongoose;
