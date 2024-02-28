d63750d5ef54cb89ac3a0e159050804ac4dc4820
Rai Yugi <raikungfu98@gmail.com>
2024-02-29 02:07:35 +0700

    test deploy 7

--- Client/src/type/constant.ts
+++ Client/src/type/constant.ts
@@ -1,4 +1,6 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_ENVIRONMENT === "prod"
-    ? import.meta.env.VITE_PROD_API_URL
-    : import.meta.env.VITE_DEV_API_URL;
+  import.meta.env.VITE_ENVIRONMENT === "dev"
+    ? import.meta.env.VITE_DEV_API_URL
+    : import.meta.env.VITE_PROD_API_URL;
+
+console.log(import.meta.env.VITE_ENVIRONMENT);
--- Server/server.js
+++ Server/server.js
@@ -14,8 +14,16 @@ app.use(cookieParser());
 const userState = require("./Routers/UserState");
 const authentication = require("./Routers/Authenticate");
 const userPosts = require("./Routers/Protected/Post");
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  if (process.env.NODE_ENV === "production") {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://socialnetworking2.onrender.com/"
+    );
+  } else {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  }
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
