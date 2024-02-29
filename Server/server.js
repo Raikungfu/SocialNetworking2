5e1d390e758dd7d07d2bddefea7b551e7c6838dc
Rai Yugi <raikungfu98@gmail.com>
2024-02-29 12:51:04 +0700

    @@@@@

--- Server/server.js
+++ Server/server.js
@@ -17,14 +17,13 @@ const authentication = require("./Routers/Authenticate");
 const userPosts = require("./Routers/Protected/Post");
 
 app.use((req, res, next) => {
-  console.log(process.env.NODE_ENV);
   if (process.env.NODE_ENV && process.env.NODE_ENV === "development") {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  } else {
     res.header(
       "Access-Control-Allow-Origin",
       "https://socialnetworkingclient.onrender.com"
     );
-  } else {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   }
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
