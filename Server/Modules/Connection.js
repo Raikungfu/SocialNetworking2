165f4e0f75fbabf8c841a905ad465ac1ff60b7d8
Rai Yugi <raikungfu98@gmail.com>
2024-03-22 11:13:27 +0700

    nnn

--- Server/Modules/Connection.js
+++ Server/Modules/Connection.js
@@ -3,8 +3,8 @@ const x = require("dotenv").config();
 
 const mongodb =
   process.env.NODE_ENV === "development"
-    ? "mongodb://127.0.0.1:27017/SocialNetworking"
-    : "mongodb+srv://raikungfu:SaintRaiYugi@cluster0.qpgarnj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    ? process.env.MONGOOSE_CONNECT_DEV
+    : process.env.MONGOOSE_CONNECT_PROD;
 
 mongoose.connect(mongodb);
 mongoose.connection.on("err", (err) => {
